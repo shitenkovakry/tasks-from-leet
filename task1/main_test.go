85a78a5326bb1c83e5fe19e138e0c3a95fd7eb2e
kry shitenkova <shitenkovakris@mail.ru>
2023-12-18 16:05:18 +0200

    do test for count oranges for task1

--- task1/main_test.go
+++ task1/main_test.go
@@ -6,12 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_AppleCount(t *testing.T) {
+func Test_AppleCount1(t *testing.T) {
 	appleLocation := 5
 	apples := []int{-2, 2, 1}
 
 	expected := []int{3,7,6}
 	actual := CountAllApples(appleLocation, apples)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_OrangeCount1(t *testing.T) {
+	orangeLocation := 15
+	oranges := []int{5, -6}
+
+	expected := []int{20,9}
+	actual := CountAllOranges(orangeLocation, oranges)
 
 	assert.Equal(t, expected, actual)
 }
